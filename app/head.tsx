fbcf7446045233cd95f63c2923988056961b8b5f
Afif Fahri <underfif@gmail.com>
2025-07-09 22:09:44 +0700

    fic icon

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>KyoStore</title>
+      <meta
+        name="description"
+        content="Solusi lengkap sosial media & aplikasi premium"
+      />
+      <meta name="apple-mobile-web-app-title" content="KyooStore" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
